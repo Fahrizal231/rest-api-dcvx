fd0a8b1b4200df7958c78c46d6675e7870d56eb9
Fahrizal231 <acioospprt@gmail.com>
2025-03-24 06:22:52 +0700

    Update index.js

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ function sendUpdateToClients() {
     });
 }
 
-const routes = ["ytdl", "twitterdl", "igdl", "fbdl", "ttdl", "githubstalk", "searchgroups", "ttsearch", "ytsearch", "llama-3.3-70b-versatile", "txt2img", "ssweb", "tahukahkamu", "brat","quotes","playstore"];
+const routes = ["ytdl", "twitterdl", "igdl", "fbdl", "ttdl", "githubstalk", "searchgroups", "ttsearch", "ytsearch", "llama-3.3-70b-versatile", "txt2img", "ssweb", "tahukahkamu", "brat","quotes","playstore","cekkhodam"];
 routes.forEach(route => {
     app.use(`/api/${route}`, require(`./api/${route}`));
 });
