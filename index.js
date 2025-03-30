a46b3c0b84c66a62d8c99e9d5829de394da59401
Fahrizal231 <acioospprt@gmail.com>
2025-03-30 13:35:16 +0700

    Update index.js

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ const routes = [
     "llama-3.3-70b-versatile", "txt2img", "ssweb", "khodam", 
     "tahukahkamu", "brat", "bratanime", "bratvid", "deepseek", 
     "alicia", "playstore", "quotes", "webtoon", "turbo", 
-    "jadwalsholat","ytmp3","gempa"
+    "jadwalsholat","ytmp3","gempa","toanime"
 ];
 
 // Cek apakah file ada sebelum memuatnya
